311f78a63810c17e6c4c53d2e52b73b77c558c80
nagyeong kim <skrud0318@naver.com>
2023-07-31 22:21:24 +0900

    router작업중..

---
+++ src/views/Router.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route, useNavigate} from 'react-router-dom';
+import Home from "./Common/Home";
+
+
+const AppRouter = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/home" element={<Home/>} />
+            </Routes>
+        </Router>
+    );
+};
+
+export default AppRouter;
