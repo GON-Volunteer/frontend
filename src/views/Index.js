561200f77a706e4e386e378206703de851d99131
nagyeong kim <skrud0318@naver.com>
2023-07-31 17:06:54 +0900

    구글 캘린더 연동 완료, home announce 작업중

--- src/views/Index.js
+++ src/views/Index.js
@@ -39,7 +39,7 @@ import SectionDark from "views/index-sections/SectionDark.js";
 import SectionLogin from "views/index-sections/SectionLogin.js";
 import SectionExamples from "views/index-sections/SectionExamples.js";
 import SectionDownload from "views/index-sections/SectionDownload.js";
-
+import Home from "views/Common/Home.js"
 function Index() {
   document.documentElement.classList.remove("nav-open");
   React.useEffect(() => {
@@ -50,9 +50,10 @@ function Index() {
   });
   return (
     <>
-      <IndexNavbar />
-      <IndexHeader />
-      <div className="main">
+      {/* <IndexNavbar />
+      <IndexHeader /> */}
+      <Home/>
+      {/* <div className="main">
         <SectionButtons />
         <SectionNavbars />
         <SectionNavigation />
@@ -67,7 +68,7 @@ function Index() {
         <SectionExamples />
         <SectionDownload />
         <DemoFooter />
-      </div>
+      </div> */}
     </>
   );
 }
