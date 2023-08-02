0da65c66bdffc3d78759fce38eb4320f33f681a7
DESKTOP-9RTH6T9\user <tgool@naver.com>
2023-08-02 16:51:12 +0900

    login 기능 구현목업 서버기준이라 실제 사용시 수정 필요

--- src/views/Common/LoginPage.js
+++ src/views/Common/LoginPage.js
@@ -1,4 +1,6 @@
 import React from "react";
+import axios from "axios"; // Axios 사용 예시
+import { useState } from "react";
 // css
 import "../../assets/css/Login.css";
 
@@ -13,6 +15,38 @@ import {
 } from "react-router-dom";
 
 function LoginPage() {
+  const [formData, setFormData] = useState({
+    id: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post(
+        "https://f12e3ca1-926d-4342-bd7c-a87451995428.mock.pstmn.io",
+        formData
+      ); // 서버로 로그인 정보 전송
+      const token = response.data.token; // 서버에서 발급된 JWT 토큰
+      localStorage.setItem("token", token); // 토큰을 로컬 저장소에 저장
+      // 로그인 성공 후, 다음 페이지로 이동
+      if (response.data.code === "400") {
+        alert("아이디 또는 비밀번호가 틀렸습니다.");
+        return;
+      }
+      navigate("/home");
+    } catch (error) {
+      console.log("로그인 실패:", error);
+    }
+  };
+
   const navigate = useNavigate();
 
   const goHome = () => {
@@ -35,20 +69,37 @@ function LoginPage() {
           />
           <Form className="register-form">
             <label id="login-text">ID</label>
-            <Input placeholder="ID" type="text" />
+            <Input
+              name="id"
+              placeholder="ID"
+              type="text"
+              value={formData.id}
+              onChange={handleChange}
+            />
             <label id="login-text">Password</label>
-            <Input placeholder="Password" type="password" />
-            <Button block className="btn-round" color="info" onClick={goHome}>
+            <Input
+              name="password"
+              placeholder="Password"
+              type="password"
+              value={formData.password}
+              onChange={handleChange}
+            />
+            <Button
+              block
+              className="btn-round"
+              color="info"
+              onClick={handleLogin}
+            >
               Login
             </Button>
           </Form>
         </div>
-      </div>
-      <div className="footer register-footer text-center">
-        <h6>
-          © {new Date().getFullYear()}, made with{" "}
-          <i className="fa fa-heart heart" /> by GON
-        </h6>
+        <div className="footer register-footer text-center">
+          <h6>
+            © {new Date().getFullYear()}, made with{" "}
+            <i className="fa fa-heart heart" /> by GON
+          </h6>
+        </div>
       </div>
     </>
   );
