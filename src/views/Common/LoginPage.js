3b95752c42974f9766b277c4a7e6bd781b1953ed
nagyeong kim <skrud0318@naver.com>
2023-08-03 11:25:06 +0900

    mockServer axios 통신 완료, redeux파일 수정 필요 userInfo{}, dispatch 핸들링 오류

--- src/views/Common/LoginPage.js
+++ src/views/Common/LoginPage.js
@@ -1,29 +1,95 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useCookies } from "react-cookie";
+import axios from "axios";
 // css
 import "../../assets/css/Login.css";
-
 // reactstrap components
 import { Button, Card, Form, Input, Container, Row, Col } from "reactstrap";
-
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   useNavigate,
 } from "react-router-dom";
+import { loginInfo } from "../redux/userSlice";
 
 function LoginPage() {
+  const formRef = useRef();
+  const [cookies, setCookie] = useCookies(["id"]);
   const navigate = useNavigate();
 
   const goHome = () => {
     navigate("/home");
   };
-  const handleLogin=(e)=>{
-    e.preventDefault();
-    const id = e.target.elements["id"].value; 
-    const password = e.target.elements["password"].value; 
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [msg, setMsg] = useState("");
+  let user = useSelector((state) => {
+    return state.user;
+  });
+  const dispatch = useDispatch();
 
-  }
+  useEffect(() => {
+    if (msg) {
+      setTimeout(() => {
+        setMsg("");
+        setLoading(false);
+      }, 1500);
+    }
+  }, [msg]);
+
+  // const handleLogin = (e) => {
+  //   e.preventDefault();
+  //   const id = e.target.elements["id"].value;
+  //   const password = e.target.elements["password"].value;
+  // };
+  const LoginFunc = (e) => {
+    e.preventDefault();
+    if (!id) {
+      return alert("Put in your ID");
+    } else if (!password) {
+      return alert("Put in your Password");
+    } else {
+      let body = {
+        id,
+        password,
+      };
+
+      axios
+        .post(
+          "https://f12e3ca1-926d-4342-bd7c-a87451995428.mock.pstmn.io",
+          body
+        )
+        .then((res) => {
+          if (res.data.code == 400) {
+            console.log(res.data);
+            console.log("로그인");
+            goHome();
+            setCookie("id", res.data.token); //cookie에 토큰저장
+
+            // const userInfo = {
+            //   success: res.data.success,
+            //   token: res.data.token,
+            //   code: res.data.code,
+            // };
+            // dispatch(loginInfo.loginUser(res.data.userInfo));
+          }
+          if (res.data.code === 200) {
+            console.log(res.data);
+            setMsg("ID, Password가 비어있습니다.");
+          }
+          if (res.data.code === 401) {
+            setMsg("존재하지 않는 ID입니다.");
+          }
+          if (res.data.code === 402) {
+            setMsg("Password가 틀립니다.");
+          }
+        });
+    }
+    setLoading(true);
+  };
   return (
     <>
       <div className="login-page">
@@ -41,15 +107,28 @@ function LoginPage() {
           />
           <Form className="register-form">
             <label id="login-text">ID</label>
-            <Input placeholder="ID" type="text" />
+            <Input
+              placeholder="ID"
+              type="text"
+              onChange={(e) => setId(e.target.value)}
+            />
             <label id="login-text">Password</label>
-            <Input placeholder="Password" type="password" />
-            <Button block className="btn-round" color="info" onClick={goHome}>
+            <Input
+              placeholder="Password"
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button
+              block
+              className="btn-round"
+              color="info"
+              disabled={loading}
+              onClick={LoginFunc}
+            >
               Login
             </Button>
           </Form>
         </div>
-
       </div>
       <div className="footer register-footer text-center">
         <h6>
