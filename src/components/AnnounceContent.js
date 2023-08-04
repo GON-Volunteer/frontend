7321490138cdb400810d707e11f7748e29cc245a
nagyeong kim <skrud0318@naver.com>
2023-08-04 14:53:59 +0900

    Register for Student Add

---
+++ src/components/AnnounceContent.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AnnounceContentContent = () => {
+  // 컴포넌트의 로직과 JS 코드
+};
+
+export default AnnounceContentContent;
--- src/store/userSlice.js
+++ src/store/userSlice.js
@@ -1,37 +1,37 @@
 import { useMediaQuery } from "@material-ui/core";
 import { createSlice } from "@reduxjs/toolkit";
-
+//success-> isAuthenticated로 바꿈 헷갈리니까
 const userSlice = createSlice({
   name: "user",
   initialState: {
     id: "",
-    accountinfo: "",
     username: "",
     success: "",
+    auth: "",
   },
   reducers: {
     loginUser: (state, action) => {
       state.id = action.payload.id;
-      state.accountinfo = action.payload.accountinfo;
       state.username = action.payload.username;
       state.success = action.payload.success;
+      state.auth = action.payload.auth;
     },
 
     clearUser: (state) => {
       state.id = "";
-      state.accountinfo = "";
       state.username = "";
       state.success = "";
+      state.auth = "";
     },
   },
 });
 
 //export const { loginUser, clearUser } = userSlice.actions;
 // export const { loginUser } = userSlice.actions;
-export const selectId = (state) => state.user.id;
-export const selectAc = (state) => state.user.accountinfo;
-export const selectname = (state) => state.user.username;
-export const selectSuccess = (state) => state.user.success;
+// export const selectId = (state) => state.user.id;
+// export const selectAc = (state) => state.user.accountinfo;
+// export const selectname = (state) => state.user.username;
+// export const selectSuccess = (state) => state.user.success;
 
 export const searchAction = userSlice.actions;
 export const searchReducer = userSlice.reducer;
