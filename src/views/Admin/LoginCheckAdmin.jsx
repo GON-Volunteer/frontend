91da0ef802fb1ea6a0df6bbce8860fcce6ee569f
DESKTOP-9RTH6T9\user <tgool@naver.com>
2023-08-04 14:56:08 +0900

    권한 확인 알고리즘 완성

--- src/views/Admin/LoginCheckAdmin.jsx
+++ src/views/Admin/LoginCheckAdmin.jsx
@@ -1,40 +1,56 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useCookies } from "react-cookie";
 import axios from "axios";
 import { searchAction } from "../store/userSlice";
-import { useNavigate } from "react-router-dom";
-function LoginCheckAdmin() {
+import { useLocation, Outlet, Navigate } from "react-router-dom";
+
+function LoginCheck() {
   const dispatch = useDispatch();
   const [cookies, setCookie, removeCookie] = useCookies(["token"]);
   const token = cookies.token;
   const user = useSelector((state) => state.user);
-  const navigate = useNavigate();
+  const location = useLocation();
+  const [renderComponent, setRenderComponent] = useState(null);
 
   useEffect(() => {
-    if (token) {
-      try {
-        const response = axios.get(
-          "https://f12e3ca1-926d-4342-bd7c-a87451995428.mock.pstmn.io/check",
-          {
-            headers: {
-              Authorization: "Bearer " + token, // 토큰을 포함한 Authorization 헤더
-            },
+    const checkAuthorization = async () => {
+      if (token) {
+        try {
+          const response = await axios.get(
+            "https://f12e3ca1-926d-4342-bd7c-a87451995428.mock.pstmn.io/check",
+            {
+              headers: {
+                Authorization: "Bearer " + token,
+              },
+            }
+          );
+          console.log(response);
+          if (response.data.code === 400 || user.auth !== "admin") {
+            dispatch(searchAction.clearUser(user));
+            alert("Not authorized");
+            setRenderComponent(
+              <Navigate to="/login" state={{ from: location }} replace />
+            );
+          } else if (response.data.code === 200) {
+            setRenderComponent(<Outlet />);
           }
-        );
-        if (response === 400 && auth != "admin") {
-          dispatch(searchAction.clearUser(user));
-          return alert("Not authorized"), navigate("/login");
+        } catch (error) {
+          console.error("Error fetching data:", error);
         }
-      } catch (error) {
-        console.error("Error fetching data:", error);
+      } else {
+        dispatch(searchAction.clearUser(user));
+        alert("Not authorized");
+        setRenderComponent(
+          <Navigate to="/login" state={{ from: location }} replace />
+        );
       }
-    } else {
-      dispatch(searchAction.clearUser(user));
-      return alert("Not authorized"), navigate("/login");
-    }
+    };
+
+    checkAuthorization();
   }, []);
 
-  return <></>;
+  return renderComponent;
 }
-export default LoginCheckAdmin;
+
+export default LoginCheck;
