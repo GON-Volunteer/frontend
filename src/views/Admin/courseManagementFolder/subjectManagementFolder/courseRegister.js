83c06f06a3bbaa40fc348b2425f45d676f3b314e
nagyeong kim <skrud0318@naver.com>
2023-08-21 20:23:01 +0900

    자잘한 오류 수정

--- src/views/Admin/courseManagementFolder/subjectManagementFolder/courseRegister.js
+++ src/views/Admin/courseManagementFolder/subjectManagementFolder/courseRegister.js
@@ -79,6 +79,9 @@ export default function CourseRegister() {
       if (response.data.code === "200") {
         // 성공적으로 추가된 경우
         setPopupVisible(true);
+        setTimeout(() => {
+          setPopupVisible(false);
+        }, 3000);
       } else if (response.data.code === "400") {
         // 실패한 경우 처리
         setErrPopupVisible(true);
@@ -88,6 +91,9 @@ export default function CourseRegister() {
     } catch (error) {
       console.error("Error adding course:", error);
       setErrPopupVisible(true);
+      setTimeout(() => {
+        setErrPopupVisible(false);
+      }, 3000);
     }
   };
   // const [batchOptions, setBatchOptions] = useState([]);
